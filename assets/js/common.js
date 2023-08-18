a082245aa1ba370af44b9ee33138d772c360d8aa
changhwanhwang <110794583+changhwanhwang@users.noreply.github.com>
2023-08-18 15:04:32 +0900

    수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -23,104 +23,104 @@ window.addEventListener('load', function(){
     .catch(error => console.log(error))                     
 });
 
-// $(()=>{
-//     _layout.init();
-//     _front.init();
-//     _aside.init();
-// })
+$(()=>{
+    _layout.init();
+    _front.init();
+    _aside.init();
+})
 
 // gnb-btn
-// const _aside = {
-//     init: function(){
-//         $(document).find(".btn__aside-open").on("click", function(){
-//             _aside.open();
-//         })
-//         $(document).find(".btn__aside-close").on("click", function(){
-//             _aside.close();
-//         })
-//     },
-//     open: function(){
-//         const elem = $(document).find(".aside__wrap");
-//         const btn_open = $(document).find(".btn__aside-open");
-//         elem.addClass("open");
-//         elem.removeAttr("aria-hidden");
-//         btn_open.attr("aria-expanded", true);
+const _aside = {
+    init: function(){
+        $(document).find(".btn__aside-open").on("click", function(){
+            _aside.open();
+        })
+        $(document).find(".btn__aside-close").on("click", function(){
+            _aside.close();
+        })
+    },
+    open: function(){
+        const elem = $(document).find(".aside__wrap");
+        const btn_open = $(document).find(".btn__aside-open");
+        elem.addClass("open");
+        elem.removeAttr("aria-hidden");
+        btn_open.attr("aria-expanded", true);
         
         
-//         $(".btn__aside-open").attr({"aria-hidden":true, "tabindex":-1});
-//         $(document).find(".aside__wrap h2").attr("tabindex", 0).focus();
+        $(".btn__aside-open").attr({"aria-hidden":true, "tabindex":-1});
+        $(document).find(".aside__wrap h2").attr("tabindex", 0).focus();
 
-//         _front.noScroll(0);
-//         if( $(".wrap").hasClass("index") ) myFullpage.setAllowScrolling(false);
-//     },
-//     close: function(){
+        _front.noScroll(0);
+        if( $(".wrap").hasClass("index") ) myFullpage.setAllowScrolling(false);
+    },
+    close: function(){
 
-//         $(document).find(".btn__aside-open").attr("tabindex", 0).focus();
-//         $(".btn__aside-open").removeAttr("aria-hidden tabindex");
+        $(document).find(".btn__aside-open").attr("tabindex", 0).focus();
+        $(".btn__aside-open").removeAttr("aria-hidden tabindex");
 
-//         const elem = $(document).find(".aside__wrap");
-//         const btn_open = $(document).find(".btn__aside-open");
-//         elem.removeClass("open");
-//         elem.attr("aria-hidden", true);
-//         btn_open.attr("aria-expanded", false);
+        const elem = $(document).find(".aside__wrap");
+        const btn_open = $(document).find(".btn__aside-open");
+        elem.removeClass("open");
+        elem.attr("aria-hidden", true);
+        btn_open.attr("aria-expanded", false);
 
-//         $(".wrap").removeAttr("aria-hidden tabindex");
+        $(".wrap").removeAttr("aria-hidden tabindex");
         
-//         _front.noScroll(1);
-//         if( $(".wrap").hasClass("index") ) myFullpage.setAllowScrolling(true);
-//     }
-// }
+        _front.noScroll(1);
+        if( $(".wrap").hasClass("index") ) myFullpage.setAllowScrolling(true);
+    }
+}
 
-// const _layout = {
-//     init:function(){
-//         _layout.header();
-//     },
-//     header: function(){
-//         const html = `
-//             <div class="header_inner">
-//                 <h1 class="logo">
-//                     <a href="#"><img src="../assets/img/logo.svg" alt="채팅 플러스 로고 이미지"></a>
-//                 </h1>
-//                 <button type="button" class="btn__aside-open" aria-expanded="false" aria-label="메뉴"><span></span></button>
-//             </div> 
-//             <div class="aside__wrap" aria-hidden="true">
-//                 <div class="aside__ttl"><h2><span class="hidden">메뉴</span></h2></div>
-//                 <div class="aside__inner">
-//                     <div class="aside__menu">
-//                         <ul>
-//                             <li><a href="#">주요서비스</a></li>
-//                             <li><a href="#">배터리 인증서</a></li>
-//                             <li><a href="#">이용안내</a></li>
-//                             <li><a href="#">제휴문의</a></li>
-//                             <li><a href="#">공지사항</a></li>
-//                             <li><a href="#">FAQ</a></li>
-//                             <li><a href="#">EV Life</a></li>
-//                             <li><a href="#">이벤트</a></li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//                 <button type="button" class="btn__aside-close" aria-label="닫기"></button>
-//             </div>
-//         `;
-//         $(document).find("header#header").html(html);
+const _layout = {
+    init:function(){
+        _layout.header();
+    },
+    header: function(){
+        const html = `
+            <div class="header_inner">
+                <h1 class="logo">
+                    <a href="#"><img src="../assets/img/logo.svg" alt="채팅 플러스 로고 이미지"></a>
+                </h1>
+                <button type="button" class="btn__aside-open" aria-expanded="false" aria-label="메뉴"><span></span></button>
+            </div> 
+            <div class="aside__wrap" aria-hidden="true">
+                <div class="aside__ttl"><h2><span class="hidden">메뉴</span></h2></div>
+                <div class="aside__inner">
+                    <div class="aside__menu">
+                        <ul>
+                            <li><a href="#">주요서비스</a></li>
+                            <li><a href="#">배터리 인증서</a></li>
+                            <li><a href="#">이용안내</a></li>
+                            <li><a href="#">제휴문의</a></li>
+                            <li><a href="#">공지사항</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">EV Life</a></li>
+                            <li><a href="#">이벤트</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <button type="button" class="btn__aside-close" aria-label="닫기"></button>
+            </div>
+        `;
+        $(document).find("header#header").html(html);
 
-//         $(document).on("click", ".header__app-btn[role='combobox']", function(e){
-//             const parents = $(this).parents(".header__app");
-//             if( parents.hasClass("on") ){
-//                 parents.removeClass("on");
-//                 $(this).attr("aria-expanded", false);
-//             } else {
-//                 parents.addClass("on");
-//                 $(this).attr("aria-expanded", true);
-//             }
-//         })
+        $(document).on("click", ".header__app-btn[role='combobox']", function(e){
+            const parents = $(this).parents(".header__app");
+            if( parents.hasClass("on") ){
+                parents.removeClass("on");
+                $(this).attr("aria-expanded", false);
+            } else {
+                parents.addClass("on");
+                $(this).attr("aria-expanded", true);
+            }
+        })
 
-//     }
-// }
+    }
+}
 
 // tab
 function tabNavItem() {
-    const tabLink = '.tab-list li button';
+    const tabLink = '.tab_inn li a';
     const tabLinkTrigers = document.querySelectorAll(tabLink);
     const tabNavPanels = document.querySelectorAll('.tab-panel');
     if(tabLinkTrigers) {
@@ -130,18 +130,18 @@ function tabNavItem() {
                 let _target = this.closest('.tab-list');
                 let _targetLinks = _target.querySelectorAll(tabLink);
                 _targetLinks.forEach(function(_targetLink) {
-                    _targetLink.parentNode.classList.remove('is-active');
+                    _targetLink.parentNode.classList.remove('active');
                     _targetLink.removeAttribute('title');
                 });
-                tabLinkTriger.parentNode.classList.add('is-active');
+                tabLinkTriger.parentNode.classList.add('active');
                 tabLinkTriger.setAttribute('title', '선택됨');
 
                 // tabNavPanel 있는 경우
                 if(tabNavPanels.length>0) {
                     tabNavPanels.forEach(function(tabNavPanel) {
-                        tabNavPanel.classList.remove('is-active');
+                        tabNavPanel.classList.remove('active');
                     });
-                    tabNavPanels[index].classList.add('is-active');
+                    tabNavPanels[index].classList.add('active');
                 }
             });
         });
@@ -151,31 +151,27 @@ tabNavItem();
 
 
 // faq
-class Accordion {
-    constructor(heading) {
-    this.heading = heading;
-}
-
-showOne() {
-    const accordionHeading = document.querySelectorAll(this.heading);
-    accordionHeading.forEach((item, key) => {
-        item.addEventListener('click', () => {
-            if (item.classList.contains("active")) {
-                item.classList.remove("active");
-            } else {
-                const accordionTitlesWithIsOpen = document.querySelectorAll(".accordion > .active");
-                accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
-                    accordionTitleWithIsOpen.classList.remove("active");
-                });
-                item.classList.add("active");
-            }
-        });
-        });
+var accordion = {
+    click: function(target) {
+      var $target = $(target);
+      $target.on('click', function() {
+  
+        if ($(this).hasClass('on')) {
+          slideUp($target);
+        } else {
+          slideUp($target);
+          $(this).addClass('on').next().slideDown();
+        }
+  
+        function slideUp($target) {
+          $target.removeClass('on').next().slideUp();
+        }
+  
+      });
     }
-}
-const accordion = new Accordion('.acc_tit');
-// for open every use showAll();
-accordion.showOne();
+};
+accordion.click('.acc_wrap > .acc_tit');
+
 
 // scroll 
 // 스크롤 모션
@@ -246,19 +242,3 @@ function scrollEventManage(){
   // 위 아래 구분을 위한 스크립트====================
 }
 
-
-
-// accdion
-// $(document).ready(function () {
-//     $(".acc_cont").hide();
-//     $(".acc_tit.acc-add").addClass("open").next().slideDown(300);
-
-//     $(".acc_tit").click(function () {
-//         if ($(this).hasClass("open")) {
-//         $(this).removeClass("open").next().slideUp(300);
-//         } else {
-//         $(".acc_tit").removeClass("open").next().slideUp(300);
-//         $(this).addClass("open").next().slideDown(300);
-//         }
-//     });
-// });

c674e962714bb3d27d0f119c91058b5b03f6a4b6
changhwanhwang <110794583+changhwanhwang@users.noreply.github.com>
2023-08-21 13:21:25 +0900

    아코디언 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -23,101 +23,6 @@ window.addEventListener('load', function(){
     .catch(error => console.log(error))                     
 });
 
-$(()=>{
-    _layout.init();
-    _front.init();
-    _aside.init();
-})
-
-// gnb-btn
-// const _aside = {
-//     init: function(){
-//         $(document).find(".btn__aside-open").on("click", function(){
-//             _aside.open();
-//         })
-//         $(document).find(".btn__aside-close").on("click", function(){
-//             _aside.close();
-//         })
-//     },
-//     open: function(){
-//         const elem = $(document).find(".aside__wrap");
-//         const btn_open = $(document).find(".btn__aside-open");
-//         elem.addClass("open");
-//         elem.removeAttr("aria-hidden");
-//         btn_open.attr("aria-expanded", true);
-        
-        
-//         $(".btn__aside-open").attr({"aria-hidden":true, "tabindex":-1});
-//         $(document).find(".aside__wrap h2").attr("tabindex", 0).focus();
-
-//         _front.noScroll(0);
-//         if( $(".wrap").hasClass("index") ) myFullpage.setAllowScrolling(false);
-//     },
-//     close: function(){
-
-//         $(document).find(".btn__aside-open").attr("tabindex", 0).focus();
-//         $(".btn__aside-open").removeAttr("aria-hidden tabindex");
-
-//         const elem = $(document).find(".aside__wrap");
-//         const btn_open = $(document).find(".btn__aside-open");
-//         elem.removeClass("open");
-//         elem.attr("aria-hidden", true);
-//         btn_open.attr("aria-expanded", false);
-
-//         $(".wrap").removeAttr("aria-hidden tabindex");
-        
-//         _front.noScroll(1);
-//         if( $(".wrap").hasClass("index") ) myFullpage.setAllowScrolling(true);
-//     }
-// }
-
-// const _layout = {
-//     init:function(){
-//         _layout.header();
-//     },
-//     header: function(){
-//         const html = `
-//             <div class="header_inner">
-//                 <h1 class="logo">
-//                     <a href="#"><img src="../assets/img/logo.svg" alt="채팅 플러스 로고 이미지"></a>
-//                 </h1>
-//                 <button type="button" class="btn__aside-open" aria-expanded="false" aria-label="메뉴"><span></span></button>
-//             </div> 
-//             <div class="aside__wrap" aria-hidden="true">
-//                 <div class="aside__ttl"><h2><span class="hidden">메뉴</span></h2></div>
-//                 <div class="aside__inner">
-//                     <div class="aside__menu">
-//                         <ul>
-//                             <li><a href="#">주요서비스</a></li>
-//                             <li><a href="#">배터리 인증서</a></li>
-//                             <li><a href="#">이용안내</a></li>
-//                             <li><a href="#">제휴문의</a></li>
-//                             <li><a href="#">공지사항</a></li>
-//                             <li><a href="#">FAQ</a></li>
-//                             <li><a href="#">EV Life</a></li>
-//                             <li><a href="#">이벤트</a></li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//                 <button type="button" class="btn__aside-close" aria-label="닫기"></button>
-//             </div>
-//         `;
-//         $(document).find("header#header").html(html);
-
-//         $(document).on("click", ".header__app-btn[role='combobox']", function(e){
-//             const parents = $(this).parents(".header__app");
-//             if( parents.hasClass("on") ){
-//                 parents.removeClass("on");
-//                 $(this).attr("aria-expanded", false);
-//             } else {
-//                 parents.addClass("on");
-//                 $(this).attr("aria-expanded", true);
-//             }
-//         })
-
-//     }
-// }
-
 // tab
 const tabList = document.querySelectorAll('.tab_wrap ul li');
 const contents = document.querySelectorAll('.tab_cont')
@@ -145,35 +50,42 @@ for(var i = 0; i < tabList.length; i++){
 
 
 // faq-accordion
-// var accordion = {
-//     click: function(target) {
-//       var $target = $(target);
-//       $target.on('click', function() {
+function bindingAccordionEvent(wrap){
+  let areaArr = document.querySelectorAll(wrap);
   
-//         if ($(this).hasClass('on')) {
-//           slideUp($target);
-//         } else {
-//           slideUp($target);
-//           $(this).addClass('on').next().slideDown();
-//         }
+  areaArr.forEach(function(area){
+    let hasGroup = area.dataset['group'] !== undefined ? true : false;
+    let btn = area.querySelector('.acc_tit');
+    
+    btn.addEventListener('click', function(){
+      if(hasGroup === true){
   
-//         function slideUp($target) {
-//           $target.removeClass('on').next().slideUp();
-//         }
-  
-//       });
-//     }
-// };
-// accordion.click('.acc_wrap > .acc_tit');
-
-const accordionItems = document.querySelectorAll(".acc_wrap");
-
-accordionItems.forEach(item =>
-  item.addEventListener("click", () => {
-    const isItemOpen = item.classList.contains("active");
-    accordionItems.forEach(item => item.classList.remove("active"));
-    if (!isItemOpen) {
-      item.classList.toggle("active");
-    }
-  })
-);
+        groupArr.forEach(function(group){
+           let content = group.querySelector('.acc_tit');
+          content.classList.remove('active');
+        });
+        thisContent.classList.add('active');
+      }else{
+        let content = area.querySelector('.acc_tit');
+        content.classList.toggle('active');
+      }
+    })
+    btn.addEventListener('click', function(){
+      if(hasGroup === true){
+        let name = area.dataset['group'];
+        let groupArr = document.querySelectorAll(wrap + '[data-group="'+ name +'"]');
+        let thisContent = area.querySelector('.acc_cont');
+        
+        groupArr.forEach(function(group){
+           let content = group.querySelector('.acc_cont');
+          content.classList.remove('active');
+        });
+        thisContent.classList.add('active');
+      }else{
+        let content = area.querySelector('.acc_cont');
+        content.classList.toggle('active');
+      }
+    });
+  });
+}
+bindingAccordionEvent('.acc_wrap');
